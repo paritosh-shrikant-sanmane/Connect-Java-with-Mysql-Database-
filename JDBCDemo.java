b7c7d22894340e603d293c83457c72eb051a5395
PARITOSH SANMANE <104440409+paritosh-shrikant-sanmane@users.noreply.github.com>
2023-11-22 15:42:31 +0530

    Add files via upload

---
+++ JDBCDemo.java
@@ -0,0 +1,33 @@
+import java.sql.*;
+
+public class JDBCDemo {
+
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/jdbcdemo";
+        String username = "root";
+        String password = "";
+
+        try {
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from student");
+
+            while (resultSet.next()) {
+
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getInt(3));
+
+            }
+            connection.close();
+
+        } catch (Exception e) {
+
+            System.out.println(e);
+        }
+    }
+}
